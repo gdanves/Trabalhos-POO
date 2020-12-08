c6f20ad3299355b3a7852cd881ff95c8498b17ab
gdanves <41815461+gdanves@users.noreply.github.com>
2020-12-08 20:03:09 -0300

    Begin N2

---
+++ N2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MathMagic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
